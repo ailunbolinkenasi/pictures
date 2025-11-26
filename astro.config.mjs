8ec8988969c2fc48c1ab8a246a7cd1bb8040fa9f
Brave <beilanzhisen@163.com>
2025-11-26 20:31:40 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import sharp from "sharp";
 
 export default defineConfig({
   site: "https://mysite.com",
@@ -8,6 +9,15 @@ export default defineConfig({
   },
   integrations: [sitemap()],
   prefetch: true,
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+      config: {
+        // 保留 EXIF 元数据
+        withMetadata: true,
+      },
+    },
+  },
   vite: {
     ssr: {
       noExternal: ["smartypants"],
